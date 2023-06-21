29c6be5b2f976089cdc7acfeebb73f797663ec78
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2023-06-21 12:51:45 +0300

    Added gitlab.yml and fixed the parent plugin compatibility issue

--- kb-addon-for-visual-composer.php
+++ kb-addon-for-visual-composer.php
@@ -2,7 +2,7 @@
 /*
   Plugin Name: KB Addon For WPBakery Page Builder
   Plugin URI:  http://codecanyon.net/item/kb-addon-for-visual-composer/14935093
-  Version: 1.0.8
+  Version: 1.0.9
   Description: Manage KB categories, tags, ask a question form, search box, tabs from WPBakery Page Builder. This addon allows you to sort categories, tags and tabs by using drag drop feature.
   Author: xenioushk
   Author URI:  http://codecanyon.net/user/xenioushk
@@ -11,8 +11,8 @@
  */
 
 // If this file is called directly, abort.
-if (!defined( 'WPINC' ) ) {
-    die;
+if (!defined('WPINC')) {
+  die;
 }
 
 /* ----------------------------------------------------------------------------*
@@ -26,29 +26,29 @@ define('BKB_VC_PARENT_PLUGIN_INSTALLED_VERSION', get_option('bwl_kb_plugin_versi
 define('BKB_VC_ADDON_PARENT_PLUGIN_TITLE', 'BWL Knowledge Base Manager Plugin');
 define('BKB_VC_ADDON_TITLE', 'KB Addon For WPBakery Page Builder');
 define('BKB_VC_PARENT_PLUGIN_REQUIRED_VERSION', '1.0.9'); // change plugin required version in here.
-define('BKB_VC_ADDON_CURRENT_VERSION', '1.0.8'); // change plugin current version in here.
+define('BKB_VC_ADDON_CURRENT_VERSION', '1.0.9'); // change plugin current version in here.
 
 define('BKB_VC_PATH', plugin_dir_path(__FILE__));
 define("BKB_VC_PLUGIN_DIR", plugins_url() . '/kb-addon-for-visual-composer/');
 
-require_once( BKB_VC_PATH . 'public/class-kbvc-addon.php' );
+require_once(BKB_VC_PATH . 'public/class-kbvc-addon.php');
 
 /*
  * Register hooks that are fired when the plugin is activated or deactivated.
  * When the plugin is deleted, the uninstall.php file is loaded.
  */
-register_activation_hook( __FILE__, array( 'BKB_VC', 'activate' ) );
-register_deactivation_hook( __FILE__, array( 'BKB_VC', 'deactivate' ) );
+register_activation_hook(__FILE__, array('BKB_VC', 'activate'));
+register_deactivation_hook(__FILE__, array('BKB_VC', 'deactivate'));
 
-add_action( 'plugins_loaded', array( 'BKB_VC', 'get_instance' ) );
+add_action('plugins_loaded', array('BKB_VC', 'get_instance'));
 
 /* ----------------------------------------------------------------------------*
  * Dashboard and Administrative Functionality
  * ---------------------------------------------------------------------------- */
 
-if ( is_admin()) {
+if (is_admin()) {
 
-//require_once( plugin_dir_path( __FILE__ ) . 'admin/class-kbdabp-addon-admin.php' );
-//add_action( 'plugins_loaded', array( 'BKB_VC_Admin', 'get_instance' ) );
+  //require_once( plugin_dir_path( __FILE__ ) . 'admin/class-kbdabp-addon-admin.php' );
+  //add_action( 'plugins_loaded', array( 'BKB_VC_Admin', 'get_instance' ) );
 
 }
