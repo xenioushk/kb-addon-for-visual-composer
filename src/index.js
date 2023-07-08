576e7c14553315c5bff863a79d95f248e5cb805b
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2023-07-08 11:03:02 +0300

    removed extra js codes

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
 import "./styles/frontend.scss"
 
 // Javascripts
-import "./modules/kb_live_search"
-import "./modules/kb_tabify"
-import "./modules/kb_question_form"
-import "./modules/kb_toc"
-import "./modules/kb_all"
+// import "./modules/kb_live_search"
+// import "./modules/kb_tabify"
+// import "./modules/kb_question_form"
+// import "./modules/kb_toc"
+// import "./modules/kb_all"
