545ec989b27272ff210e72f77870e5ae0857f2e2
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2024-12-10 15:54:00 +0200

    Fixed notices for WordPress 6.7.1

--- assets/scripts/admin.js
+++ assets/scripts/admin.js
@@ -1,1 +1,275 @@
-(()=>{var t={574:()=>{jQuery(document).ready((function(t){t((function(){setTimeout((function(){t("span[data-vc-ui-element=button-save]").on("click",(function(){var e,n;e="",n=0,t(".bkb_cat").find("li").each((function(){e+=t(this).data("value")+",",n++})),n>0&&(e=e.substr(0,e.length-1)),t(".kb_cat").val("").val(e)}))}),0),t("#sortable1, #sortable2").sortable({connectWith:".connectedSortable"}).disableSelection()}))}))},665:()=>{jQuery(document).ready((function(t){t((function(){setTimeout((function(){t("span[data-vc-ui-element=button-save]").on("click",(function(){var e,n;e="",n=0,t(".bkb_counter").find("li").each((function(){e+=t(this).data("value")+",",n++})),n>0&&(e=e.substr(0,e.length-1)),t(".kb_counter").val("").val(e)}))}),0),t("#sortable1").sortable().disableSelection()}))}))},981:()=>{jQuery(document).ready((function(t){t((function(){setTimeout((function(){t("span[data-vc-ui-element=button-save]").on("click",(function(){var e,n;e="",n=0,t(".bkb_tabs").find("li").each((function(){e+=t(this).data("value")+",",n++})),n>0&&(e=e.substr(0,e.length-1)),t(".kb_tabs").val("").val(e)}))}),0),t("#sortable1").sortable().disableSelection()}))}))},673:()=>{jQuery(document).ready((function(t){t((function(){setTimeout((function(){t("span[data-vc-ui-element=button-save]").on("click",(function(){var e,n;e="",n=0,t(".bkb_tags").find("li").each((function(){e+=t(this).data("value")+",",n++})),n>0&&(e=e.substr(0,e.length-1)),t(".kb_tags").val("").val(e)}))}),0),t("#sortable1, #sortable2").sortable({connectWith:".connectedSortable"}).disableSelection()}))}))},108:()=>{var t;t=jQuery,void 0!==BkbmKavcAdminData.installation&&1!=BkbmKavcAdminData.installation&&t.when(t.ajax({type:"POST",url:ajaxurl,data:{action:"bkbm_kavc_installation_counter",product_id:BkbmKavcAdminData.product_id},dataType:"JSON"})).done((function(t){}))}},e={};function n(a){var o=e[a];if(void 0!==o)return o.exports;var i=e[a]={exports:{}};return t[a](i,i.exports,n),i.exports}n.n=t=>{var e=t&&t.__esModule?()=>t.default:()=>t;return n.d(e,{a:e}),e},n.d=(t,e)=>{for(var a in e)n.o(e,a)&&!n.o(t,a)&&Object.defineProperty(t,a,{enumerable:!0,get:e[a]})},n.o=(t,e)=>Object.prototype.hasOwnProperty.call(t,e),(()=>{"use strict";n(108),n(574),n(665),n(981),n(673)})()})();
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/admin/modules/bkb_cat_sort.js":
+/*!*******************************************!*\
+  !*** ./src/admin/modules/bkb_cat_sort.js ***!
+  \*******************************************/
+/***/ (() => {
+
+jQuery(document).ready(function ($) {
+  $(function () {
+    function _bkb_cat_lists() {
+      var output = '';
+      var count = 0;
+      $('.bkb_cat').find('li').each(function () {
+        output += $(this).data('value') + ',';
+        count++;
+      });
+      if (count > 0) {
+        output = output.substr(0, output.length - 1);
+      }
+      $('.kb_cat').val("").val(output);
+    }
+    setTimeout(function () {
+      $("span[data-vc-ui-element=button-save]").on("click", function () {
+        _bkb_cat_lists();
+      });
+    }, 0);
+    $("#sortable1, #sortable2").sortable({
+      connectWith: ".connectedSortable"
+    }).disableSelection();
+  });
+});
+
+/***/ }),
+
+/***/ "./src/admin/modules/bkb_counter_sort.js":
+/*!***********************************************!*\
+  !*** ./src/admin/modules/bkb_counter_sort.js ***!
+  \***********************************************/
+/***/ (() => {
+
+jQuery(document).ready(function ($) {
+  $(function () {
+    function _bkb_counter_lists() {
+      var output = '';
+      var count = 0;
+      $('.bkb_counter').find('li').each(function () {
+        output += $(this).data('value') + ',';
+        count++;
+      });
+      if (count > 0) {
+        output = output.substr(0, output.length - 1);
+      }
+      $('.kb_counter').val("").val(output);
+    }
+    setTimeout(function () {
+      $("span[data-vc-ui-element=button-save]").on("click", function () {
+        _bkb_counter_lists();
+      });
+    }, 0);
+    $("#sortable1").sortable().disableSelection();
+  });
+});
+
+/***/ }),
+
+/***/ "./src/admin/modules/bkb_tabs_sort.js":
+/*!********************************************!*\
+  !*** ./src/admin/modules/bkb_tabs_sort.js ***!
+  \********************************************/
+/***/ (() => {
+
+jQuery(document).ready(function ($) {
+  $(function () {
+    function _bkb_tabs_lists() {
+      var output = '';
+      var count = 0;
+      $('.bkb_tabs').find('li').each(function () {
+        output += $(this).data('value') + ',';
+        count++;
+      });
+      if (count > 0) {
+        output = output.substr(0, output.length - 1);
+      }
+      $('.kb_tabs').val("").val(output);
+    }
+    setTimeout(function () {
+      $("span[data-vc-ui-element=button-save]").on("click", function () {
+        _bkb_tabs_lists();
+      });
+    }, 0);
+    $("#sortable1").sortable().disableSelection();
+  });
+});
+
+/***/ }),
+
+/***/ "./src/admin/modules/bkb_tags_sort.js":
+/*!********************************************!*\
+  !*** ./src/admin/modules/bkb_tags_sort.js ***!
+  \********************************************/
+/***/ (() => {
+
+jQuery(document).ready(function ($) {
+  $(function () {
+    function _bkb_tags_lists() {
+      var output = '';
+      var count = 0;
+      $('.bkb_tags').find('li').each(function () {
+        output += $(this).data('value') + ',';
+        count++;
+      });
+      if (count > 0) {
+        output = output.substr(0, output.length - 1);
+      }
+      $('.kb_tags').val("").val(output);
+    }
+    setTimeout(function () {
+      $("span[data-vc-ui-element=button-save]").on("click", function () {
+        _bkb_tags_lists();
+      });
+    }, 0);
+    $("#sortable1, #sortable2").sortable({
+      connectWith: ".connectedSortable"
+    }).disableSelection();
+  });
+});
+
+/***/ }),
+
+/***/ "./src/admin/modules/installation_counter.js":
+/*!***************************************************!*\
+  !*** ./src/admin/modules/installation_counter.js ***!
+  \***************************************************/
+/***/ (() => {
+
+;
+(function ($) {
+  function bkbm_kavc_installation_counter() {
+    return $.ajax({
+      type: "POST",
+      url: ajaxurl,
+      data: {
+        action: "bkbm_kavc_installation_counter",
+        // this is the name of our WP AJAX function that we'll set up next
+        product_id: BkbmKavcAdminData.product_id // change the localization variable.
+      },
+
+      dataType: "JSON"
+    });
+  }
+  if (typeof BkbmKavcAdminData.installation != "undefined" && BkbmKavcAdminData.installation != 1) {
+    $.when(bkbm_kavc_installation_counter()).done(function (response_data) {
+      // console.log(response_data)
+    });
+  }
+})(jQuery);
+
+/***/ }),
+
+/***/ "./src/admin/styles/admin.scss":
+/*!*************************************!*\
+  !*** ./src/admin/styles/admin.scss ***!
+  \*************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be in strict mode.
+(() => {
+"use strict";
+/*!**********************************!*\
+  !*** ./src/admin/admin_index.js ***!
+  \**********************************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _styles_admin_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles/admin.scss */ "./src/admin/styles/admin.scss");
+/* harmony import */ var _modules_installation_counter__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/installation_counter */ "./src/admin/modules/installation_counter.js");
+/* harmony import */ var _modules_installation_counter__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_modules_installation_counter__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _modules_bkb_cat_sort__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./modules/bkb_cat_sort */ "./src/admin/modules/bkb_cat_sort.js");
+/* harmony import */ var _modules_bkb_cat_sort__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_modules_bkb_cat_sort__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _modules_bkb_counter_sort__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./modules/bkb_counter_sort */ "./src/admin/modules/bkb_counter_sort.js");
+/* harmony import */ var _modules_bkb_counter_sort__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_modules_bkb_counter_sort__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var _modules_bkb_tabs_sort__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./modules/bkb_tabs_sort */ "./src/admin/modules/bkb_tabs_sort.js");
+/* harmony import */ var _modules_bkb_tabs_sort__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_modules_bkb_tabs_sort__WEBPACK_IMPORTED_MODULE_4__);
+/* harmony import */ var _modules_bkb_tags_sort__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./modules/bkb_tags_sort */ "./src/admin/modules/bkb_tags_sort.js");
+/* harmony import */ var _modules_bkb_tags_sort__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(_modules_bkb_tags_sort__WEBPACK_IMPORTED_MODULE_5__);
+// Load Stylesheets.
+
+
+// Load JavaScripts
+
+
+
+
+
+})();
+
+/******/ })()
+;
+//# sourceMappingURL=admin.js.map
--- kb-addon-for-visual-composer.php
+++ kb-addon-for-visual-composer.php
@@ -2,7 +2,7 @@
 /*
   Plugin Name: KB Addon For WPBakery Page Builder
   Plugin URI:  https://1.envato.market/bkbm-wp
-  Version: 1.1.4
+  Version: 1.1.5
   Description: Manage KB categories, tags, ask a question form, search box, tabs from WPBakery Page Builder. This addon allows you to sort categories, tags and tabs by using drag drop feature.
   Author: xenioushk
   Author URI:  https://bluewindlab.net
@@ -22,7 +22,7 @@ define('BKB_VC_PARENT_PLUGIN_INSTALLED_VERSION', get_option('bwl_kb_plugin_versi
 define('BKB_VC_ADDON_PARENT_PLUGIN_TITLE', 'BWL Knowledge Base Manager Plugin');
 define('BKB_VC_ADDON_TITLE', 'KB Addon For WPBakery Page Builder');
 define('BKB_VC_PARENT_PLUGIN_REQUIRED_VERSION', '1.4.2'); // change plugin required version in here.
-define('BKB_VC_ADDON_CURRENT_VERSION', '1.1.4'); //Plugin current version.
+define('BKB_VC_ADDON_CURRENT_VERSION', '1.1.5'); //Plugin current version.
 define('BKB_VC_ADDON_INSTALLATION_TAG', 'bkbm_kavc_installation_' . str_replace('.', '_', BKB_VC_ADDON_CURRENT_VERSION));
 define('BKB_VC_PATH', plugin_dir_path(__FILE__));
 define("BKB_VC_PLUGIN_DIR", plugins_url() . '/kb-addon-for-visual-composer/');
@@ -30,9 +30,20 @@ define('BKB_VC_ADDON_UPDATER_SLUG', plugin_basename(__FILE__)); // change plugin
 define("BKB_VC_ADDON_CC_ID", "14935093"); // Plugin codecanyon Id.
 
 // Pre activated plugin for knowledgedesk theme
-$currentTheme = trim(wp_get_theme());
+$kdeskBundle = 0;
 
-if ($currentTheme == "knowledgedesk" || $currentTheme == "knowledgedesk Child") {
+if (function_exists('wp_get_theme')) {
+  // Get the current active theme info
+  $currentTheme = \wp_get_theme();
+  // Make a list of all allowed themes.
+  $allowedThemes = ['knowledgedesk', 'knowledgedesk Child'];
+  // Checking if current theme exists in the allowed theme list.
+  if (in_array($currentTheme->get('Name'), $allowedThemes, true)) {
+    $kdeskBundle = 1;
+  }
+}
+
+if ($kdeskBundle == 1) {
   $purchaseVerified = 1;
 } else if (get_option('bkbm_purchase_verified') == 1) {
   $purchaseVerified = 1;
