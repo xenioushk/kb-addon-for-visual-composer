1c0ab878d7b80046aa36bf986219e7620ba2afc7
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2025-04-10 16:33:11 +0300

    ❌ Removed:Extra files. 🔥 Updated: Plugin tabs callback function.

--- includes/Callbacks/Shortcodes/TabsCb.php
+++ includes/Callbacks/Shortcodes/TabsCb.php
@@ -38,20 +38,20 @@ class TabsCb {
 		$bkb_tabs = explode( ',', $tabs );
 		$limit    = isset( $limit ) ? (int) $limit : 5;
 
-		$bkb_tab_shortcode_string = '[bkb_tabs vertical=' . $vertical . ' rtl=' . $rtl . ' cont_ext_class=' . $cont_ext_class . ' ]';
+		$shortcode_string = '[bkb_tabs vertical=' . $vertical . ' rtl=' . $rtl . ' cont_ext_class=' . $cont_ext_class . ' ]';
 
 		foreach ( $bkb_tabs as $tab_title ) {
 
 			switch ( $tab_title ) {
 
 				case 'featured':
-						$bkb_tab_shortcode_string .= '[bkb_tab title="' . $feat_tab_title . '"][bwl_kb bkb_tabify="1" meta_key="bkb_featured_status" meta_value="1" orderby="meta_value_num" order="DESC" limit="' . $limit . '" bkb_list_type="' . $bkb_list_type . '"][/bkb_tab]';
+						$shortcode_string .= '[bkb_tab title="' . $feat_tab_title . '"][bwl_kb bkb_tabify="1" meta_key="bkb_featured_status" meta_value="1" orderby="meta_value_num" order="DESC" limit="' . $limit . '" bkb_list_type="' . $bkb_list_type . '"][/bkb_tab]';
 			        break;
 				case 'popular':
-						$bkb_tab_shortcode_string .= '[bkb_tab title="' . $popular_tab_title . '"][bwl_kb bkb_tabify="1" meta_key="bkbm_post_views" orderby="meta_value_num" order="DESC" limit="' . $limit . '" bkb_list_type="' . $bkb_list_type . '"][/bkb_tab]';
+						$shortcode_string .= '[bkb_tab title="' . $popular_tab_title . '"][bwl_kb bkb_tabify="1" meta_key="bkbm_post_views" orderby="meta_value_num" order="DESC" limit="' . $limit . '" bkb_list_type="' . $bkb_list_type . '"][/bkb_tab]';
 			        break;
 				case 'recent':
-						$bkb_tab_shortcode_string .= '[bkb_tab title="' . $recent_tab_title . '"] [bwl_kb bkb_tabify="1" orderby="ID" order="DESC" limit="' . $limit . '" bkb_list_type="' . $bkb_list_type . '"][/bkb_tab]';
+						$shortcode_string .= '[bkb_tab title="' . $recent_tab_title . '"] [bwl_kb bkb_tabify="1" orderby="ID" order="DESC" limit="' . $limit . '" bkb_list_type="' . $bkb_list_type . '"][/bkb_tab]';
 			        break;
 
 				default:
@@ -59,8 +59,8 @@ class TabsCb {
 			}
 		}
 
-		$bkb_tab_shortcode_string .= '[/bkb_tabs]';
+		$shortcode_string .= '[/bkb_tabs]';
 
-		return do_shortcode( $bkb_tab_shortcode_string );
+		return do_shortcode( $shortcode_string );
 	}
 }
